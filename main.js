419ad334bb262f4d53c0ec770b2735442e9eecf1
Valeria Preciado <preciadovaleriam@gmail.com>
2023-08-16 13:48:07 -0600

    Funcion del carrito de compras

--- main.js
+++ main.js
@@ -2,16 +2,39 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
-
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const asideClosed = aside.classList.contains('inactive');
+
+    if (!asideClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu (){
-mobileMenu.classList.toggle('inactive');
+    const asideClosed = aside.classList.contains('inactive');
+
+    if (!asideClosed){
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside (){
+    const mobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!mobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+    
+} 
